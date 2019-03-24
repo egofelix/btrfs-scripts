9dc4d00c3a88d77761d2701e4b29276341ec8d38
Felix Wederz <git@egofelix.de>
2019-03-24 17:45:59 +0100

    Initial commit

---
+++ archlinux.sh
@@ -0,0 +1,331 @@
+#!/bin/bash
+
+# Parse arguments
+CRYPTED=""
+TARGET_SYSTEM="debian"
+DEV_ROOT="/dev/sda"
+DEV_ETC=""
+DEV_HOME=""
+DEV_OPT=""
+DEV_SRV=""
+DEV_USR=""
+DEV_VAR=""
+FILESYS="btrfs"
+VERBOSE=""
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+      --verbose) export VERBOSE=" --verbose"; shift 1;;
+      --system) export TARGET_SYSTEM="$2"; shift 2;;
+      --root) export DEV_ROOT="$2"; shift 2;;
+      --var) export DEV_VAR="$2"; shift 2;;
+      --usr) export DEV_USR="$2"; shift 2;;
+      --home) export DEV_HOME="$2"; shift 2;;
+      --srv) export DEV_SRV="$2"; shift 2;;
+      --opt) export DEV_OPT="$2"; shift 2;;
+      --etc) export DEV_ETC="$2"; shift 2;;
+      --crypt) export CRYPTED="true"; shift 1;;
+
+      -*) echo "unknown option: $1" >&2; exit 1;;
+       *) echo "unknown option: $1" >&2; exit 1;;
+  esac
+done
+
+function getSystemType {
+  DISTIDENTIFIER=`uname -m`
+  if [ "${DISTIDENTIFIER^^}" = "ARMV7L" ]; then
+    echo -n "ARMHF";
+  else
+    echo -n "AMD64";
+  fi;
+}
+
+function getSystemName {
+  SYSNAME=`uname -a`
+  if [[ "${SYSNAME^^}" = *"DEBIAN"* ]]; then
+    echo -n "DEBIAN";
+  elif [[ "${SYSNAME^^}" = *"ARCH"* ]]; then
+    echo -n "ARCHLINUX";
+  else
+    echo -n "UNKNOWN";
+  fi;
+}
+
+function installPackage {
+  if [[ ( "${3}" != "" ) && ( "${2}" = "" || $(getSystemName) == "${2^^}" ) ]]; then
+    EXISTS=`type ${3} >/dev/null 2>&1 || echo "Not Installed"`
+    if [[ "${EXISTS}" = "" ]]; then
+      echo ${1} is already installed...
+      return 0
+    fi;
+  fi;
+
+  if [[ ( "${2}" = "" && $(getSystemName) = "DEBIAN" ) || ( $(getSystemName) == "${2^^}" ) ]]; then
+    echo Installing ${1}...
+    DEBIAN_FRONTEND=noninteractive apt-get update -qq && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends $1 -qq > /dev/null
+  fi;
+
+  if [[ ( "${2}" = "" && $(getSystemName) = "ARCHLINUX" ) || ( $(getSystemName) == "${2^^}" ) ]]; then
+    echo Installing ${1}...
+    pacman -Sy --noconfirm ${1}
+  fi;
+
+  return 0
+}
+
+function formatPartition {
+  FILESYSFORCE="-F"
+  if [[ "${3^^}" = "BTRFS" ]]; then
+    FILESYSFORCE="-f"
+  fi;
+
+  if [[ "${4^^}" = "TRUE" ]]; then
+    if [[ ! -z "${5}" ]]; then
+      cryptsetup --batch-mode luksFormat --type luks1 ${1} -d ${5}
+      cryptsetup --batch-mode open ${1} crypt${2} -d ${5}
+    else
+      echo test1234 | cryptsetup --batch-mode luksFormat --type luks1 ${1}
+      echo test1234 | cryptsetup --batch-mode open ${1} crypt${2}
+    fi;
+
+    mkfs.${3,,} ${FILESYSFORCE} -L ${2} /dev/mapper/crypt${2}
+    sync
+  else
+    mkfs.${3,,} ${FILESYSFORCE} -L ${2} ${1}
+    sync
+  fi;
+
+
+  if [[ "${3^^}" = "BTRFS" ]]; then
+    mkdir -p /tmp/btrfs/${2}
+
+    if [[ "${4^^}" = "TRUE" ]]; then
+      mount /dev/mapper/crypt${2} /tmp/btrfs/${2}
+    else
+      mount ${1} /tmp/btrfs/${2}
+    fi;
+    btrfs subvol create /tmp/btrfs/${2}/data
+    btrfs subvol set-default `btrfs subvol list /tmp/btrfs/${2} | grep data | cut -d' ' -f2` /tmp/btrfs/${2}
+    sync
+    umount /tmp/btrfs/${2}
+    sync
+  fi;
+}
+
+function formatDrive {
+  sfdisk ${1} <<- EOM
+label: gpt
+unit: sectors
+
+start=        2048, size=      204800, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="${2}"
+EOM
+  sync
+
+  # Resize Root Partition
+  parted ${1} resizepart 1 100%
+  sync
+
+  # Create Filesystem
+  if [[ "${4^^}" = "TRUE" ]]; then
+    formatPartition ${1}1 ${2} ${3} ${4} /mnt/crypt.key
+  else
+    formatPartition ${1}1 ${2} ${3} ${4}
+  fi;
+
+  # Mount
+  mkdir /mnt/${2}
+  if [[ "${4^^}" = "TRUE" ]]; then
+    mount /dev/mapper/crypt${2} /mnt/${2}
+  else
+    mount ${1}1 /mnt/${2}
+  fi;
+
+  sync
+}
+
+if [[ ! -d "/sys/firmware/efi" ]]; then
+  echo "BIOS IS NOT SUPPORTED"
+  exit 1
+fi;
+
+if [[ "${TARGET_SYSTEM^^}" != "DEBIAN" ]]; then
+  echo "Only supported target system is debian atm"
+  exit 1
+fi;
+
+installPackage debootstrap "" debootstrap;
+installPackage parted "" parted;
+
+# Format Disk
+sfdisk ${DEV_ROOT} <<- EOM
+label: gpt
+unit: sectors
+
+start=        2048, size=      204800, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="efi"
+start=      206848, size=      512000, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="boot"
+start=      718848, size=      204800, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="root"
+EOM
+sync
+
+# Resize Root Partition
+parted ${DEV_ROOT} resizepart 3 100%
+sync
+
+# Create Filesystems
+mkfs.vfat -F32 ${DEV_ROOT}1
+fatlabel ${DEV_ROOT} EFI
+mkfs.ext2 -F -L boot ${DEV_ROOT}2
+formatPartition ${DEV_ROOT}3 root ${FILESYS} ${CRYPTED}
+sync
+
+# Mount Root
+if [[ "${CRYPTED^^}" = "TRUE" ]]; then
+  mount /dev/mapper/cryptroot /mnt
+else
+  mount ${DEV_ROOT}3 /mnt
+fi;
+
+# Mount Boot and Efi
+mkdir /mnt/boot
+mount ${DEV_ROOT}2 /mnt/boot
+mkdir /mnt/boot/efi
+mount ${DEV_ROOT}1 /mnt/boot/efi
+sync
+
+# Generate a key file
+if [[ "${CRYPTED^^}" = "TRUE" ]]; then
+  dd if=/dev/urandom of=/mnt/crypt.key bs=1024 count=1
+  echo test1234 | cryptsetup --batch-mode luksAddKey ${DEV_ROOT}3 /mnt/crypt.key
+fi;
+
+# Format Additional Partitions?
+if [[ ! -z "${DEV_ETC}" ]];  then formatDrive ${DEV_ETC} etc ${FILESYS} ${CRYPTED}; fi;
+if [[ ! -z "${DEV_HOME}" ]]; then formatDrive ${DEV_HOME} home ${FILESYS} ${CRYPTED}; fi;
+if [[ ! -z "${DEV_OPT}" ]];  then formatDrive ${DEV_OPT} opt ${FILESYS} ${CRYPTED}; fi;
+if [[ ! -z "${DEV_SRV}" ]];  then formatDrive ${DEV_SRV} srv ${FILESYS} ${CRYPTED}; fi;
+if [[ ! -z "${DEV_USR}" ]];  then formatDrive ${DEV_USR} usr ${FILESYS} ${CRYPTED}; fi;
+if [[ ! -z "${DEV_VAR}" ]];  then formatDrive ${DEV_VAR} var ${FILESYS} ${CRYPTED}; fi;
+
+# Install Base to /mnt
+echo "Installing Base System..."
+
+# Install Strap
+if [[ "${TARGET_SYSTEM^^}" = "DEBIAN" ]]; then
+  debootstrap stretch /mnt http://ftp.de.debian.org/debian/;
+else
+  echo "Could not install system"
+  exit 1
+fi;
+
+# Generate fstab
+genfstab -pL /mnt >> /mnt/etc/fstab
+
+# Additional Mounts for chroot
+echo "Mountint additional Mounts";
+mount -t proc proc /mnt/proc/
+mount -t sysfs sys /mnt/sys/
+mount -t devtmpfs dev /mnt/dev/
+mount -t devpts devpts /mnt/dev/pts
+mount -t efivarfs efivarfs /mnt/sys/firmware/efi/efivars
+
+# Setup Network
+# Setup Systemd-Networkd interface
+cat > /mnt/etc/systemd/network/*.network <<- EOM
+[Match]
+Name=ens192
+
+[Network]
+DHCP=yes
+EOM
+
+# Script Header
+cat > /mnt/chrootinit.sh <<- EOM
+#!/bin/bash
+. /etc/profile
+EOM
+
+# Update System
+cat >> /mnt/chrootinit.sh <<- EOM
+DEBIAN_FRONTEND=noninteractive apt-get update -qq
+EOM
+
+# Install Locales
+cat >> /mnt/chrootinit.sh <<- EOM
+apt-get install -y locales
+sed -i '/en_US.UTF-8/s/^#//' /etc/locale.gen
+locale-gen en_US.UTF-8
+EOM
+
+# Set Root Password
+cat >> /mnt/chrootinit.sh <<- EOM
+echo -e "root\nroot" | passwd root
+EOM
+
+# Install linux-image
+cat >> /mnt/chrootinit.sh <<- EOM
+DEBIAN_FRONTEND=noninteractive apt-get install -y -qq linux-image-amd64
+EOM
+
+if [[ "${FILESYS^^}" = "BTRFS" ]]; then
+cat >> /mnt/chrootinit.sh <<- EOM
+DEBIAN_FRONTEND=noninteractive apt-get install -y -qq btrfs-progs
+EOM
+fi;
+
+# Install Bootloader
+cat >> /mnt/chrootinit.sh <<- EOM
+DEBIAN_FRONTEND=noninteractive apt-get install -y -qq grub2-common grub-efi
+grub-install
+grub-mkconfig -o /boot/grub/grub.cfg
+EOM
+
+if [[ "${CRYPTED^^}" = "TRUE" ]]; then
+  # Install cryptsetup and dropbear
+  cat >> /mnt/chrootinit.sh <<- EOM
+chown root:root /crypt.key
+chmod 600 /crypt.key
+DEBIAN_FRONTEND=noninteractive apt-get install -y -qq cryptsetup #dropbear-initramfs
+echo cryptroot /dev/sda3 none luks,allow-discards > /etc/crypttab
+EOM
+
+  # Additional Drives?
+  if [[ ! -z "${DEV_ETC}" ]];  then echo "echo cryptetc ${DEV_ETC}1 /crypt.key luks,allow-discards >> /etc/crypttab" >> /mnt/chrootinit.sh; fi;
+  if [[ ! -z "${DEV_HOME}" ]];  then echo "echo crypthome ${DEV_HOME}1 /crypt.key luks,allow-discards >> /etc/crypttab" >> /mnt/chrootinit.sh; fi;
+  if [[ ! -z "${DEV_OPT}" ]];  then echo "echo cryptopt ${DEV_OPT}1 /crypt.key luks,allow-discards >> /etc/crypttab" >> /mnt/chrootinit.sh; fi;
+  if [[ ! -z "${DEV_SRV}" ]];  then echo "echo cryptsrv ${DEV_SRV}1 /crypt.key luks,allow-discards >> /etc/crypttab" >> /mnt/chrootinit.sh; fi;
+  if [[ ! -z "${DEV_USR}" ]];  then echo "echo cryptusr ${DEV_USR}1 /crypt.key luks,allow-discards >> /etc/crypttab" >> /mnt/chrootinit.sh; fi;
+  if [[ ! -z "${DEV_VAR}" ]];  then echo "echo cryptvar ${DEV_VAR}1 /crypt.key luks,allow-discards >> /etc/crypttab" >> /mnt/chrootinit.sh; fi;
+
+  # Setup btrfs module for initramfs
+  if [[ "${FILESYS^^}" = "BTRFS" ]]; then
+    cat >> /mnt/chrootinit.sh <<- EOM
+echo "btrfs" >> /etc/initramfs-tools/modules
+EOM
+  fi;
+
+  # Update initramfs & grub
+  cat >> /mnt/chrootinit.sh <<- EOM
+update-initramfs -k all -u
+grub-mkconfig -o /boot/grub/grub.cfg
+EOM
+fi;
+
+# Enable Systemd-Networkd
+cat >> /mnt/chrootinit.sh <<- EOM
+systemctl enable systemd-networkd
+EOM
+
+# Install sshd
+cat >> /mnt/chrootinit.sh <<- EOM
+DEBIAN_FRONTEND=noninteractive apt-get install -y -qq openssh-server
+EOM
+
+# Call chrootinit.sh in chroot
+chmod +x /mnt/chrootinit.sh
+chroot /mnt /chrootinit.sh;
+
+# Cleanup
+rm /mnt/chrootinit.sh
+sync
+umount -R /mnt
+reboot now
