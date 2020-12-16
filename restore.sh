1b851c85e1e1bd89f69b2a210cec801697904be1
Felix <git@egofelix.de>
2020-12-16 13:55:54 +0100

    Work...

--- restore.sh
+++ restore.sh
@@ -9,7 +9,7 @@ source "${BASH_SOURCE%/*}/functions.sh"
 # Mount backup drive?
 SNAPSOURCE=$(LANG=C mount | grep -v '/dev/mapper/cryptsystem on .* type btrfs' | grep 'type btrfs' | grep -o 'on .* type btrfs' | awk '{print $2}')
 if [[ -z "${SNAPSOURCE}" ]]; then
-	if [[ ! test -e /dev/disk/by-label/backup ]]; then
+	if [[ ! -e /dev/disk/by-label/backup ]]; then
 		logLine "Cannot find backup directory, please attach backup drive.";
 		exit;
 	fi;
