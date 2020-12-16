6f959da3bdd253b1ebc5653d9ad97c47d974c924
felix <felix>
2020-12-16 11:17:44 +0100

    Work

--- createBackup.sh
+++ createBackup.sh
@@ -11,10 +11,12 @@ STAMP=`date -u +"%Y-%m-%d-%H-%M-%S"`
 
 # Search snapshot volume
 SNAPDIR=`LANG=C mount | grep snapshots | grep -o 'on /\..* type btrfs' | awk '{print $2}'`
-if [[ ! -z "${SNAPDIR}" ]]; then
+if [[ -z "${SNAPDIR}" ]]; then
 	logLine "Cannot find snapshot directory";
 	exit;
 fi;
+
+logLine "Target Directory: ${SNAPDIR}";
 
 # Finish
 logLine "Backup done.";
