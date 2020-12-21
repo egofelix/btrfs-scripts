0d61f85a165baa930bb15d3635fd9918369c311c
Felix <git@egofelix.de>
2020-12-21 18:17:49 +0100

    Work

--- sendBackups.sh
+++ sendBackups.sh
@@ -110,8 +110,8 @@ do
 	if [ $? -ne 0 ]; then logLine "Failed to run ssh command: check-volume-backup "${volName}" "${FIRSTSUBVOLUME}"" exit 1; fi;
 	if isFalse ${SUBVOLUME_EXISTS}; then
 		logLine "Sending backup \"${volName}_${FIRSTSUBVOLUME}\" (Full)";
-		echo "btrfs send ${SNAPSOURCE}/${volName}/${FIRSTSUBVOLUME}"
-		echo ${SSH_CALL} create-volume-backup "${volName}" "${FIRSTSUBVOLUME}";
+		#echo "btrfs send ${SNAPSOURCE}/${volName}/${FIRSTSUBVOLUME}"
+		#echo ${SSH_CALL} create-volume-backup "${volName}" "${FIRSTSUBVOLUME}";
 		SENDRESULT=$(btrfs send ${SNAPSOURCE}/${volName}/${FIRSTSUBVOLUME} | ${SSH_CALL} create-volume-backup "${volName}" "${FIRSTSUBVOLUME}")
 		if [[ $? -ne 0 ]] || [[ "${SENDRESULT}" != "success" ]]; then logLine "Failed to send backup."; exit 1; fi;
 	fi;
