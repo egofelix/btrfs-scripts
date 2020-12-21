ca3ed55f79282a491dbc97aa993323fa3405417b
Felix <git@egofelix.de>
2020-12-21 17:14:45 +0100

    Work

--- backupReceiver.sh
+++ backupReceiver.sh
@@ -24,7 +24,7 @@ fi;
 
 if [[ "$1" = "check-volume-backup" ]]; then
   # Check Argument Count
-  if [ [ -z "$2" ] || [ -z "$3" ] ]; then echo "Usage: check-volume-backup volume backup"; exit 1; fi;
+  if [[ -z "$2" ]] || [[ -z "$3" ]]; then echo "Usage: check-volume-backup volume backup"; exit 1; fi;
   
   # Check volume parameter
   if [[ $2 = *"." ]]; then echo "Illegal character . detected in parameter volume."; exit 1;  fi;
