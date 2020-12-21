669ae72734f3f72328e7000b646ba4f67e2c9de3
Felix <git@egofelix.de>
2020-12-21 18:05:29 +0100

    Work

--- backupReceiver.sh
+++ backupReceiver.sh
@@ -82,7 +82,7 @@ if [[ "$1" = "create-volume-backup" ]]; then
   if [[ -d "${HOME}/$2/$3" ]]; then echo "backup already exists"; exit 0; fi;
   
   # Receive  
-  echo Redirecting input to btrfs receive -v ${HOME}/$2
+  echo "RECEIVER: Redirecting input to btrfs receive -v ${HOME}/$2"
   btrfs receive -v ${HOME}/$2 <&0
   if [ $? -ne 0 ]; then 
     # Remove broken backup
