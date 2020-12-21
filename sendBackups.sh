14768366562a85e8f6204b8603a5f4137dcdd97f
Felix <git@egofelix.de>
2020-12-21 16:27:17 +0100

    Work

--- sendBackups.sh
+++ sendBackups.sh
@@ -59,9 +59,13 @@ if [[ ${SNAPTARGET} = "ssh://"* ]]; then
 	fi;
 	
 	SSH_HOSTNAME="${SSH_PART}"
+	SSH_PATH=$(echo "${SNAPTARGET}" | awk -F'/' '{print $4-}')
+	SSH_PATH="/${SSH_PATH}"
+	
 	logLine "SSH-Host: ${SSH_HOSTNAME}"
 	logLine "SSH-Port: ${SSH_PORT}"
 	logLine "SSH-User: ${SSH_USERNAME}"
+	logLine "SSH-Path: ${SSH_PATH}"
 	exit
 	
 else
