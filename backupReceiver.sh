dd63f08c476d7781e17c8772bd31dde345babea6
Felix <git@egofelix.de>
2020-12-21 18:21:38 +0100

    Work

--- backupReceiver.sh
+++ backupReceiver.sh
@@ -83,9 +83,9 @@ if [[ "$1" = "create-volume-backup" ]]; then
   
   # Receive  
   echo "RECEIVER: Redirecting input to btrfs receive -v ${HOME}/$2"
-  cat - < /dev/stdin
-  cat - <&0
-  btrfs receive -v ${HOME}/$2 <&0
+  #cat - < /dev/stdin
+  #cat - <&0
+  btrfs receive -v ${HOME}/$2 < /dev/stdin
   if [ $? -ne 0 ]; then 
     # Remove broken backup
     btrfs subvol del ${HOME}/$2/$3
