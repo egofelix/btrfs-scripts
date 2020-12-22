df4392dda43fd6da880d8c4c70e7308d2c4d4b27
Felix <git@egofelix.de>
2020-12-22 11:41:28 +0100

    Work

--- includes/functions/strings.sh
+++ includes/functions/strings.sh
@@ -4,3 +4,8 @@ function removeTrailingChar {
   if [[ "${1}" = *"${2}" ]]; then RESULT="${RESULT::-1}"; fi;
   echo "${RESULT}";
 }
+
+function isEmpty {
+  if [[ -z "$1" ]]; then return 1; fi;
+  return 0;
+}
