4f88cf15e1bafd76ec1803925f78b86d7159bf9e
Felix <git@egofelix.de>
2020-12-22 20:22:11 +0100

    Work

--- ssh_restore.sh
+++ ssh_restore.sh
@@ -171,110 +171,24 @@ cat "${FSTABPATH}" | grep -v -P '^[\s]*#' | grep -v -P '^[\s]*$' | while read LI
   fi;
 done;
 
-exit 1;
-
-
-# Mount regarding to fstab
-if ! runCmd mkdir -p /tmp/mnt/root; then logError "Failed to create root mountpoint"; exit 1; fi;
-MOUNTRESULT=$(mount --fstab "${FSTABPATH}" --target-prefix "/tmp/mnt/root" --source /dev/mapper/cryptsystem -a > /dev/null);
-if [[ $? -ne 0 ]]; then logLine "Failed to mount: ${MOUNTRESULT}."; exit 1; fi;
-
-exit 1;
-
-# Check root volumes
-SUBVOLUMES=$(${SSH_CALL} "list-volumes" | sort)
-if [[ $? -ne 0 ]]; then
-  logLine "Unable to query root volume.";
-  logLine "${SUBVOLUMES}";
-  exit 1;
-fi;
-
-
-
-
-
-
-if ! runCmd mkdir /tmp/mnt/disks/system/snapshots/root; then logLine "Failed to create snapshot directory for root."; exit 1; fi;
-
-# Receive ROOT-Volume
-logLine "Receiving ROOT-Snapshot...";
-${SSH_CALL} "receive-volume-backup" "root" "${RESTOREPOINT}" | btrfs receive -q /tmp/mnt/disks/system/snapshots/root
-if [[ $? -ne 0 ]]; then logLine "Failed to receive volume."; exit 1; fi;
-
-# Restore root-data from snapshot
-logLine "Restoring ROOT-Volume...";
-btrfs subvol snapshot /tmp/mnt/disks/system/snapshots/root/${RESTOREPOINT} /tmp/mnt/disks/system/root-data > /dev/null
-if [ $? -ne 0 ]; then logLine "Failed to restore ROOT-Volume from ROOT-Snapshot..."; exit 1; fi;
-
-# Mount ROOT-Volume
-logLine "Mounting..."
-if ! runCmd mkdir -p /tmp/mnt/root; then echo "Failed to create root mount directory"; exit 1; fi;
-if ! runCmd mount -o subvol=/root-data ${PART_SYSTEM} /tmp/mnt/root; then echo "Failed to Mount Subvolume ROOT-DATA at /tmp/mnt/root"; exit 1; fi;
-if ! runCmd mkdir -p /tmp/mnt/root/boot; then echo "Failed to create boot directory"; exit 1; fi;
-if ! runCmd mount ${PART_BOOT} /tmp/mnt/root/boot; then echo "Failed to mount BOOT-Partition"; exit 1; fi;
-if ! runCmd mkdir -p /tmp/mnt/root/.snapshots; then echo "Failed to create snapshot mount point"; exit 1; fi;
-if ! runCmd mount -o subvol=/snapshots ${PART_SYSTEM} /tmp/mnt/root/.snapshots; then echo "Failed to Mount Snapshot-Volume at /tmp/mnt/root/.snapshots"; exit; fi;
-if isEfiSystem; then
-  if ! runCmd mkdir -p /tmp/mnt/root/boot/efi; then echo "Failed to create efi directory"; exit 1; fi;
-  if ! runCmd mount ${PART_EFI} /tmp/mnt/root/boot/efi; then echo "Failed to mount BOOT-Partition"; exit 1; fi;
-fi;
-
-# Now scan fstab as root is restored! (Sort so we will mount "lowest" points first!)
-BTRFSMOUNTPOINTS=$(cat /tmp/mnt/root/etc/fstab | grep -e '\sbtrfs\s.*subvol\=' | awk '{print $2}' | grep -v '^\/$' | sort)
-for mountpoint in ${BTRFSMOUNTPOINTS}
-do
-  VOLUMENAME=$(cat /tmp/mnt/root/etc/fstab | grep "${mountpoint}" | grep -o -P 'subvol\=[^\s]*' | awk -F'=' '{print $2}')
-  if [[ -z ${VOLUMENAME} ]]; then
-    logLine "Unable to find Volume-Name for ${mountpoint}.";
-    exit 1;
-  fi;
-
-  if [[ ${VOLUMENAME} = "@"* ]]; then
-    logDebug "Creating empty volume for ${mountpoint} at ${VOLUMENAME}.";
-    if ! runCmd btrfs subvolume create /tmp/mnt/disks/system/${VOLUMENAME}; then echo "Failed to create btrfs ${VOLUMENAME^^}-Volume"; exit; fi;
-    continue;
-  fi;
-	
-  # Double check volume name against fstab
-  VOLNAME="${mountpoint//[\/]/-}"
-  VOLNAME=${VOLNAME:1}
-  if [[ "${VOLNAME^^}-DATA" != "${VOLUMENAME^^}" ]]; then
-    logDebug "Failed to detect volume for ${mountpoint}. Volume Name Mismacht! \"${VOLNAME^^}-DATA\" != \"${VOLUMENAME^^}\"";
-    exit 1;
-  fi;
-
-  # Receive XXX-Volume
-  if ! runCmd mkdir /tmp/mnt/disks/system/snapshots/${VOLNAME,,}; then logLine "Failed to create snapshot directory for ${VOLNAME,,}."; exit 1; fi;
-  logLine "Receiving ${VOLNAME^^}-Snapshot...";
-  ${SSH_CALL} "receive-volume-backup" "${VOLNAME,,}" "${RESTOREPOINT}" | btrfs receive -q /tmp/mnt/disks/system/snapshots/${VOLNAME,,}
-  if [[ $? -ne 0 ]]; then logLine "Failed to receive volume."; exit 1; fi;
-	
-  # Restore XXX-data from snapshot
-  logLine "Restoring ${VOLNAME^^}-Volume...";
-  btrfs subvol snapshot /tmp/mnt/disks/system/snapshots/${VOLNAME,,}/${RESTOREPOINT} /tmp/mnt/disks/system/${VOLNAME,,}-data > /dev/null
-  if [ $? -ne 0 ]; then logLine "Failed to restore ${VOLNAME^^}-Volume from ${VOLNAME^^}-Snapshot..."; exit 1; fi;
-	
-  # Mount it for later use
-  if ! runCmd mount -o subvol=${VOLNAME,,}-data ${PART_SYSTEM} /tmp/mnt/root${mountpoint}; then echo "Failed to Mount Subvolume ${VOLNAME^^} at /tmp/mnt/root${mountpoint}"; exit; fi;
-done;
-
-# Message
-logLine "Backups restored, finishing system restore...";
-
 # Reinstall new crypto keys and backup header
 if isTrue "${CRYPTED}"; then
+  logDebug "Installing new crypto key...";
   if ! runCmd cp /tmp/crypto.key /tmp/mnt/root/etc/; then logLine "Failed to copy crypto.key"; exit; fi;
   if ! runCmd cp /tmp/crypto.header /tmp/mnt/root/etc/; then logLine "Failed to copy crypto.header"; exit; fi;
 fi;
 
 # Prepare ChRoot
+logDebug "Preparing chroot...";
 rm -f /tmp/mnt/root/etc/resolv.conf;
 source "${BASH_SOURCE%/*}/scripts/chroot_prepare.sh";
 
 # Reinstall Packages
+logDebug "Restoring packages...";
 source "${BASH_SOURCE%/*}/scripts/restorepackages.sh";
 
 # Reinstall BootManager
+logDebug "Restoring Bootmanager...";
 source "${BASH_SOURCE%/*}/scripts/bootmanager.sh";
 
 exit 0;
