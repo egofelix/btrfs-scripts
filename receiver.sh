981977dd3766ef90fb60a107fa7257157dc8cf3c
Felix <git@egofelix.de>
2020-12-22 15:12:21 +0100

    Work

--- receiver.sh
+++ receiver.sh
@@ -57,7 +57,7 @@ function containsIllegalCharacter {
 }
 
 echo ${COMMAND};
-if containsIllegalCharacter ${COMMAND}; then logError "Illegal character detected in \"$1\"."; exit 1; fi;
+if containsIllegalCharacter ${COMMAND}; then logError "Illegal character detected in \"$1\"."; exit 1; else logError "asdasd"; fi;
 logDebug "Bllaaaa";
 exit 1;
 # We must have first parameter
