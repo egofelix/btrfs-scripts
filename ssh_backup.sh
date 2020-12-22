b0a0b7e8a264a5f653f65983e539c820592becbb
Felix <git@egofelix.de>
2020-12-22 14:23:20 +0100

    Work

--- ssh_backup.sh
+++ ssh_backup.sh
@@ -109,7 +109,7 @@ if [[ "${COMMAND,,}" = "send" ]]; then
 	CHECKVOLUMERESULT=$(${SSH_CALL} check-volume "${VOLUME}" "${FIRSTSNAPSHOT}");
 	if [ $? -ne 0 ]; then logError "Command 'check-volume \"${VOLUME}\" \"${FIRSTSNAPSHOT}\"' failed: ${CHECKVOLUMERESULT}."; exit 1; fi;
 	if isFalse ${CHECKVOLUMERESULT}; then
-	  logLine "Sending backup \"${VOLUME}\"-\"${FIRSTSNAPSHOT}\"... (Full)";
+	  logLine "Sending snapshot \"${FIRSTSNAPSHOT}\" for volume \"${VOLUME}\"... (Full)";
 	  SENDRESULT=$(btrfs send -q ${SNAPSHOTSPATH}/${VOLUME}/${FIRSTSNAPSHOT} | ${SSH_CALL} receive-volume "${VOLUME}" "${FIRSTSNAPSHOT}")
 	  if [[ $? -ne 0 ]] || [[ "${SENDRESULT}" != "success" ]]; then logError "Command 'receive-volume \"${VOLUME}\" \"${FIRSTSNAPSHOT}\"' failed: ${SENDRESULT}"; exit 1; fi;
 	fi;
