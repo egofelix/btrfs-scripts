d8a2cbdb3613b63a296f204c3d57dc3fe82d4931
Felix <git@egofelix.de>
2020-12-22 15:50:21 +0100

    Work

--- receiver.sh
+++ receiver.sh
@@ -34,7 +34,7 @@ done
 
 # Helper function
 function containsIllegalCharacter {
-  local ILLEGALCHARACTERS=("." "$" "&" "(" ")" "{" "}" "[" "]" ";" "<" ">" "\`" "|" "*" "?" "\"" "'" "*")
+  local ILLEGALCHARACTERS=("." "$" "&" "(" ")" "{" "}" "[" "]" ";" "<" ">" "\`" "|" "*" "?" "\"" "'" "*" "\\" "/")
   for CHAR in "${ILLEGALCHARACTERS[@]}"
   do
     if [[ "$1" == *"${CHAR}"* ]]; then return 0; fi;
@@ -58,6 +58,7 @@ if [[ ! ${SNAPSHOTMOUNTVOLUME} = "@"* ]] && [[ ! ${SNAPSHOTMOUNTVOLUME} = "/@"* 
 
 # Test if command is illegal
 if containsIllegalCharacter "${COMMAND}"; then logError "Illegal character detected in \"${COMMAND}\"."; exit 1; fi;
+COMMAND_NAME=$(echo "${COMMAND}" | awk '{print $1}')
 
 ## Script must be started as root
 if [[ "$EUID" -ne 0 ]]; then logError "Please run as root"; exit 1; fi;
@@ -66,155 +67,96 @@ if [[ "$EUID" -ne 0 ]]; then logError "Please run as root"; exit 1; fi;
 LOCKFILE="${SNAPSHOTSPATH}/$(basename $BASH_SOURCE)"
 source "${BASH_SOURCE%/*}/includes/lockfile.sh";
 
-
-COMMAND_NAME=$(echo "${COMMAND}" | awk '{print $1}')
+# Command testreceiver
 if [[ "${COMMAND_NAME,,}" = "testreceiver" ]]; then
   echo "success";
   exit 0;
 fi;
 
-logError "Unknown Command: ${COMMAND_NAME}.";
-exit 1;
-
-
-
-exit 1;
-# We must have first parameter
-if [[ -z "$1" ]]; then
-	echo "Missing Home dir in first paramete";
-	exit 1;
-fi;
-
-
-
-# Update home for this script
-HOME=$1
-shift 1;
-
-# Test if home is on btrfs and @ mount
-MOUNTPOINT=$(findmnt -n -o SOURCE --target ${HOME})
-if [[ $? -ne 0 ]] || [[ -z "${MOUNTPOINT}" ]]; then 
-  echo "Could not find MOUNTPOINT for ${HOME}."; exit 1;
-fi;
-MOUNTDEVICE=$(echo "${MOUNTPOINT}" | awk -F'[' '{print $1}')
-if [[ -z "${MOUNTDEVICE}" ]]; then 
-  echo "Could not find MOUNTDEVICE for ${MOUNTPOINT}."; exit 1;
-fi;
-MOUNTVOLUME=$(echo "${MOUNTPOINT}" | awk -F'[' '{print $2}' | awk -F']' '{print $1}')
-if [[ -z "${MOUNTVOLUME}" ]]; then 
-  echo "Could not find MOUNTVOLUME for ${MOUNTPOINT}."; exit 1;
-fi;
-if [[ ! ${MOUNTVOLUME} = *"@"* ]]; then
-  echo "Could not backup to ${HOME} as this does not lie on a @ subvolume."; exit 1;
-fi;
-
-# We must have first parameter
-if [[ -z "$1" ]]; then
-	echo "Missing command";
-	exit 1;
-fi;
-
-# Main Commands
-if [[ "$1" = "testReceiver" ]]; then
-  echo "success"; exit 0;
-fi;
-
-# create-volume ensures that the directory exists
-if [[ "$1" = "create-volume" ]]; then
-  # Check Argument Count
-  if [[ -z "$2" ]]; then echo "Usage: create-volume <volume>"; exit 1; fi;
- 
-  # Check volume parameter
-  if [[ $2 = *"."* ]]; then echo "Illegal character . detected in parameter volume."; exit 1;  fi;
+# Command create-volume
+if [[ "${COMMAND_NAME,,}" = "create-volume" ]]; then
+  # Test <volume> parameter
+  VOLUME=$(echo "${COMMAND}" | awk '{print $2}');
+  if [[ -z "${VOLUME}" ]]; then logError "Usage: create-volume <volume>"; exit 1; fi;
   
   # Create directory
-  if ! runCmd mkdir -p ${HOME}/$2; then echo "Error creating volume directory."; exit 1; fi;
+  if ! runCmd mkdir -p ${SNAPSHOTSPATH}/$2; then logError "Failed to create volume directory."; exit 1; fi;
   echo "success"; exit 0;
 fi;
 
-# check-volume checks if a snapshot exists
-if [[ "$1" = "check-volume" ]]; then
-  # Check Argument Count
-  if [[ -z "$2" ]] || [[ -z "$3" ]]; then echo "Usage: check-volume <volume> <name>"; exit 1; fi;
-  
-  # Check volume parameter
-  if [[ $2 = *"."* ]]; then echo "Illegal character . detected in parameter <volume>."; exit 1;  fi;
-  
-  # Check name parameter
-  if [[ $3 = *"."* ]]; then echo "Illegal character . detected in parameter <name>."; exit 1;  fi;
+# Command check-volume
+if [[ "${COMMAND_NAME,,}" = "check-volume" ]]; then
+  # Test <volume> and <name> parameter
+  VOLUME=$(echo "${COMMAND}" | awk '{print $2}');
+  NAME=$(echo "${COMMAND}" | awk '{print $3}');
+  if [[ -z "${VOLUME}" ]] || [ -z "${NAME}" ]]; then logError "Usage: check-volume <volume> <name>"; exit 1; fi;
   
   # Test and return
-  if [[ ! -d "${HOME}/$2/$3" ]]; then echo "false"; exit 0; fi;
+  if [[ ! -d "${SNAPSHOTSPATH}/${VOLUME}/${NAME}" ]]; then echo "false"; exit 0; fi;
   echo "true"; exit 0;
 fi;
 
-# receive-volume will receive a snapshot
-if [[ "$1" = "receive-volume" ]]; then
-  # Check Argument Count
-  if [[ -z "$2" ]] || [[ -z "$3" ]]; then echo "Usage: receive-volume <volume> <name>"; exit 1; fi;
-  
-  # Check volume parameter
-  if [[ $2 = *"."* ]]; then echo "Illegal character . detected in parameter <volume>."; exit 1; fi;
-  
-  # Check name parameter
-  if [[ $3 = *"."* ]]; then echo "Illegal character . detected in parameter <name>."; exit 1; fi;
+# Command list-volumes
+if [[ "${COMMAND_NAME,,}" = "list-volumes" ]]; then
+  # list directory
+  RESULT=$(ls ${SNAPSHOTSPATH});
+  if [[ $? -ne 0 ]]; then logError "listing volumes failed: ${RESULT}."; exit 1; fi;
+  echo "${RESULT}"; exit 0;
+fi;
 
+# Command list-volume
+if [[ "${COMMAND_NAME,,}" = "list-snapshots" ]]; then
+  # Test <volume> parameter
+  VOLUME=$(echo "${COMMAND}" | awk '{print $2}') 
+  if [[ -z "${VOLUME}" ]]; then logError "Usage: list-snapshots <volume>"; exit 1; fi;
+  
+  # Create directory
+  RESULT=$(ls ${SNAPSHOTSPATH}/${VOLUME});
+  if [[ $? -ne 0 ]]; then logError "listing snapshots of volume \"${VOLUME}\" failed: ${RESULT}."; exit 1; fi;
+  echo "${RESULT}"; exit 0;
+fi;
+
+# Command upload-volume
+if [[ "${COMMAND_NAME,,}" = "upload-snapshot" ]]; then
+  # Test <volume> and <name> parameter
+  VOLUME=$(echo "${COMMAND}" | awk '{print $2}');
+  NAME=$(echo "${COMMAND}" | awk '{print $3}');
+  if [[ -z "${VOLUME}" ]] || [ -z "${NAME}" ]]; then logError "Usage: upload-snapshot <volume> <name>"; exit 1; fi;
+  
   # Check if the snapshot exists already
-  if [[ -d "${HOME}/$2/$3" ]]; then echo "already exists"; exit 0; fi;
+  if [[ -d "${SNAPSHOTSPATH}/${VOLUME}/${NAME}" ]]; then logError "already exists"; exit 1; fi;
   
   # Receive  
-  btrfs receive -q ${HOME}/$2 < /dev/stdin
-  if [ $? -ne 0 ]; then 
-    # Remove broken backup
-    btrfs subvol del ${HOME}/$2/$3
+  RESULT=$(btrfs receive -q ${SNAPSHOTSPATH}/${VOLUME} < /dev/stdin);
+  if [[ $? -ne 0 ]]; then
+	# Remove broken backup
+	# TODO, Detect name from RESULT here, otherwise the user could maybe delete snaps from the remote
+    REMOVERESULT=$(btrfs subvol del ${SNAPSHOTSPATH}/${VOLUME}/${NAME});
 	
 	# Return error
-    echo "receive failed";
-	exit 1;
+	logError "failed to receive the volume: ${RESULT}."; exit 1;
   fi;
   
-  # Backup received
+  # Snapshot received
   echo "success"; exit 0;
 fi;
 
-if [[ "$1" = "list-volumes" ]]; then
-  # Create directory
-  RESULT=$(ls ${HOME})
-  if [ $? -ne 0 ]; then echo "Error listing volumes."; exit 1; fi;
+# Command download-volume
+if [[ "${COMMAND_NAME,,}" = "download-snapshot" ]]; then
+  # Test <volume> and <name> parameter
+  VOLUME=$(echo "${COMMAND}" | awk '{print $2}');
+  NAME=$(echo "${COMMAND}" | awk '{print $3}');
+  if [[ -z "${VOLUME}" ]] || [ -z "${NAME}" ]]; then logError "Usage: download-snapshot <volume> <name>"; exit 1; fi;
+
+  # Check if snapshot exists
+  if [[ ! -d "${SNAPSHOTSPATH}/${VOLUME}/${NAME}" ]]; then logError "snapshot does not exists"; exit 1; fi;
+
+  # Send Snapshot
+  btrfs send -q ${SNAPSHOTSPATH}/${VOLUME}/${NAME};
+  if [[ $? -ne 0 ]]; then logError "Error sending snapshot."; exit 1; fi;
   exit 0;
 fi;
 
-if [[ "$1" = "list-volume" ]]; then
-  # Check Argument Count
-  if [[ -z "$2" ]]; then echo "Usage: list-volume volume"; exit 1; fi;
- 
-  # Check volume parameter
-  if [[ $2 = *"."* ]]; then echo "Illegal character . detected in parameter volume."; exit 1;  fi;
-  
-  # Create directory
-  ls ${HOME}/$2
-  if [ $? -ne 0 ]; then echo "Error listing volume $2."; exit 1; fi;
-  exit 0;
-fi;
-
-if [[ "$1" = "receive-volume-backup" ]]; then
-  # Check Argument Count
-  if [[ -z "$2" ]] || [[ -z "$3" ]]; then echo "Usage: receive-volume-backup volume backup"; exit 1; fi;
-  
-  # Check volume parameter
-  if [[ $2 = *"."* ]]; then echo "Illegal character . detected in parameter volume."; exit 1;  fi;
-  
-  # Check backup parameter
-  if [[ $3 = *"."* ]]; then echo "Illegal character . detected in parameter backup."; exit 1;  fi;
-
-  # Check if backup exists
-  if [[ ! -d "${HOME}/$2/$3" ]]; then echo "backup does not exists"; exit 0; fi;
-
-  # Send Backup
-  btrfs send -q ${HOME}/$2/$3
-  if [ $? -ne 0 ]; then echo "Error sending volumes."; exit 1; fi;
-  exit 0;
-fi;
-
-echo "Error in command $@";
+# Unknown command
+logError "Unknown Command: ${COMMAND_NAME}.";
 exit 1;
