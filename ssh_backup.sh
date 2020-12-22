86b9deff88a6eb3be3966e48498d34d46d23703a
Felix <git@egofelix.de>
2020-12-22 13:04:26 +0100

    Work

--- ssh_backup.sh
+++ ssh_backup.sh
@@ -55,6 +55,7 @@ LOCKFD=99
 _lock()             { flock -$1 $LOCKFD; }
 _no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
 _prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
 _prepare_locking
 exlock_now || exit 1
 
