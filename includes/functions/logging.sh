a57e2b8453120b8b506d3ba91de5d22ff3956380
Felix <git@egofelix.de>
2020-12-23 12:18:26 +0100

    Work

--- includes/functions/logging.sh
+++ includes/functions/logging.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 function logDebug {
   if isTrue "${DEBUG:-}"; then
-    echo "[DEBUG] $@";
+    echo -en "\033[01;95m" && echo -n "[DEBUG] $@" && echo -e "\e[0m";
   fi;
 }
 
 function logWarn {
-  echo "[WARN] $@";
+  echo -en "\033[01;93m" && echo -n "[WARN] $@" && echo -e "\e[0m";
 }
 
 function logLine {
@@ -15,5 +15,5 @@ function logLine {
 }
 
 function logError {
-  echo "[ERROR] $@";
+  echo -en "\033[01;31m" && echo -n "[ERROR] $@" && echo -e "\e[0m";
 }
