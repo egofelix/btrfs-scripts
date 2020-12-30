d4f838089f5662d9c78ae4bfef524bf87a40bff5
Felix <git@egofelix.de>
2020-12-30 10:41:12 +0100

    Another Fix

--- includes/functions/booleans.sh
+++ includes/functions/booleans.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 function isTrue {
+  if [[ $# -eq 0 ]]; then return 0; fi;
   if [[ "${1^^}" = "YES" ]]; then return 0; fi;
   if [[ "${1^^}" = "TRUE" ]]; then return 0; fi;
   return 1;
 }
 function isFalse {
+  if [[ $# -eq 0 ]]; then return 0; fi;
   if [[ "${1^^}" = "NO" ]]; then return 0; fi;
   if [[ "${1^^}" = "FALSE" ]]; then return 0; fi;
   return 1;
