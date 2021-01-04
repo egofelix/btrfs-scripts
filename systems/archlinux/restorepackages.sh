53ec4fcd30f223df4565e1cf0be23754aefacffd
Felix <git@egofelix.de>
2021-01-04 15:17:13 +0100

    Work

--- systems/archlinux/restorepackages.sh
+++ systems/archlinux/restorepackages.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 cat > /tmp/mnt/root/chroot.sh <<- EOF
 #!/bin/bash
-for package in \$(pacman -Qqe);
-do
-  echo Reinstalling \$package;
-  pacman -S --noconfirm \$package;
-done;
+pacman -Qqe | xargs pacman -S --noconfirm
 EOF
 chmod +x /tmp/mnt/root/chroot.sh;
 chroot /tmp/mnt/root /chroot.sh;
