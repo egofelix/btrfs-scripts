8b639a5ae0818654707034bf69acbaef167f83b6
Felix <git@egofelix.de>
2021-01-08 15:55:48 +0100

    Preparements for Debian

--- scripts/restorepackages.sh
+++ scripts/restorepackages.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [[ $(getSystemName) = "ARCHLINUX" ]]; then
-	source "${BASH_SOURCE%/*}/../systems/archlinux/restorepackages.sh"
-else
-	logLine "Unknown system detected... Aborting"
-	exit 1;
-fi;
+source "${BASH_SOURCE%/*}/../systems/archlinux/restorepackages.sh"
