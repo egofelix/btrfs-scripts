e18976efe649c7a23e8402fd173f323cb8ab6e1f
Felix <git@egofelix.de>
2021-01-08 11:15:47 +0100

    Preparements for Debian

--- systems/ubuntu/bootmanager.sh
+++ systems/ubuntu/bootmanager.sh
@@ -1,90 +1,26 @@
 #!/bin/bash
-if [[ ( $(getSystemType) = "ARMHF" ) ]]; then
-	logLine "Setting up Bootmanager (UBOOT)";
+logLine "Setting up Bootmanager (GRUB)";
 
-    # Install uboot
-	cat > /tmp/mnt/root/chroot.sh <<- EOF
+# Install Grub
+cat > /tmp/mnt/root/chroot.sh <<- EOF
 #!/bin/bash
-yes n | pacman -S --noconfirm uboot-cubietruck uboot-tools
-mkinitcpio -P
+DEBIAN_FRONTEND=noninteractive apt-get -yq install grub efibootmgr
 EOF
-    
-	chmod +x /tmp/mnt/root/chroot.sh;
-	chroot /tmp/mnt/root /chroot.sh;
-	
-	# Setup boot.txt for seperated boot partition
-	if [ -f "/tmp/mnt/root/etc/boot.txt" ]; then
-		rm -f /tmp/mnt/root/boot/boot.txt
-		cp /tmp/mnt/root/etc/boot.txt /tmp/mnt/root/boot/boot.txt
-	else
-		if isTrue "${CRYPTED}"; then
-			cat > /tmp/mnt/root/boot/boot.txt <<- EOF
-# After modifying, run ./mkscr
 
-setenv bootpart 1;
-setenv bootargs cryptdevice=PARTLABEL=system:cryptsystem root=/dev/mapper/cryptsystem rootflags=subvol=root-data ip=:::::eth0:dhcp rw rootwait console=\${console};
+chmod +x /tmp/mnt/root/chroot.sh;
+chroot /tmp/mnt/root /chroot.sh;
 
-if load \${devtype} \${devnum}:\${bootpart} \${kernel_addr_r} /zImage; then
-  if load \${devtype} \${devnum}:\${bootpart} \${fdt_addr_r} /dtbs/\${fdtfile}; then
-	if load \${devtype} \${devnum}:\${bootpart} \${ramdisk_addr_r} /initramfs-linux.img; then
-	  bootz \${kernel_addr_r} \${ramdisk_addr_r}:\${filesize} \${fdt_addr_r};
-	else
-	  bootz \${kernel_addr_r} - \${fdt_addr_r};
-	fi;
-  fi;
-fi	
-EOF
-		else
-			cat > /tmp/mnt/root/boot/boot.txt <<- EOF
-# After modifying, run ./mkscr
+if isTrue "${CRYPTED}"; then
+	# Setup Grub for Cryptsetup
+	sed -i "s/#GRUB_ENABLE_CRYPTODISK/GRUB_ENABLE_CRYPTODISK/g" /tmp/mnt/root/etc/default/grub
+	sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptdevice=PARTLABEL=system:cryptsystem ip=:::::eth0:dhcp\"/g" /tmp/mnt/root/etc/default/grub
+fi;
 
-setenv bootpart 1;
-setenv bootargs console=${console} root=PARTLABEL=system rw rootwait;
-
-if load \${devtype} \${devnum}:\${bootpart} \${kernel_addr_r} /zImage; then
-  if load \${devtype} \${devnum}:\${bootpart} \${fdt_addr_r} /dtbs/\${fdtfile}; then
-    if load \${devtype} \${devnum}:\${bootpart} \${ramdisk_addr_r} /initramfs-linux.img; then
-      bootz \${kernel_addr_r} \${ramdisk_addr_r}:\${filesize} \${fdt_addr_r};
-    else
-      bootz \${kernel_addr_r} - \${fdt_addr_r};
-    fi;
-  fi;
-fi	
-EOF
-		fi;
-	fi;
-
-	# Recompile boot.txt -> boot.scr
-	cat > /tmp/mnt/root/chroot.sh <<- EOF
-#!/bin/bash
-cd /boot
-./mkscr
-EOF
-	chroot /tmp/mnt/root /chroot.sh;
-else
-	logLine "Setting up Bootmanager (GRUB)";
-	
-    # Install Grub
-	cat > /tmp/mnt/root/chroot.sh <<- EOF
-#!/bin/bash
-pacman -S --noconfirm grub efibootmgr
-EOF
-    
-	chmod +x /tmp/mnt/root/chroot.sh;
-	chroot /tmp/mnt/root /chroot.sh;
-
-    if isTrue "${CRYPTED}"; then
-		# Setup Grub for Cryptsetup
-		sed -i "s/#GRUB_ENABLE_CRYPTODISK/GRUB_ENABLE_CRYPTODISK/g" /tmp/mnt/root/etc/default/grub
-		sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptdevice=PARTLABEL=system:cryptsystem ip=:::::eth0:dhcp\"/g" /tmp/mnt/root/etc/default/grub
-	fi;
-
-	# Setup Grub
-	cat > /tmp/mnt/root/chroot.sh <<- EOF
+# Setup Grub
+cat > /tmp/mnt/root/chroot.sh <<- EOF
 #!/bin/bash
 mkinitcpio -P
 grub-install
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
-	chroot /tmp/mnt/root /chroot.sh;
-fi;
+chroot /tmp/mnt/root /chroot.sh;
