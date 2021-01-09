218ec1da8c7356a89478acec2abb039b16b74138
Felix <git@egofelix.de>
2021-01-09 09:28:37 +0100

    /boot partition type

--- scripts/drive_prepare.sh
+++ scripts/drive_prepare.sh
@@ -8,7 +8,7 @@ unit: sectors
 
 start=2048, size=20480, type=21686148-6449-6E6F-744E-656564454649, bootable
 start=22528, size=204800, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="efi"
-start=227328, size=512000, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="boot"
+start=227328, size=512000, type=BC13C2FF-59E6-4262-A352-B275FD6F7172, name="boot"
 start=739328, size=512000, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="system"
 EOM
 
