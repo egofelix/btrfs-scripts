8a322d11fdb545cbc40ec29eac943f3e782c6b81
Felix <git@egofelix.de>
2021-01-09 09:08:52 +0100

    Use generic partitioning scheme

--- scripts/drive_prepare.sh
+++ scripts/drive_prepare.sh
@@ -2,52 +2,27 @@
 
 # Format drives
 logLine "Partitioning ${DRIVE_ROOT}..."
-
-if [[ "${BIOSTYPE}" == "EFI" ]]; then
-	logLine "Using EFI partition scheme...";
-	sfdisk -q ${DRIVE_ROOT} &> /dev/null <<- EOM
-label: gpt
-unit: sectors
-
-start=        2048, size=      204800, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="efi"
-start=      206848, size=      512000, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="boot"
-start=      718848, size=      204800, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="system"
-EOM
-
-	# Check Result
-	if [ $? -ne 0 ]; then
-		logLine "Failed to partition the drive! Aborting"
-		exit
-	fi;
-	
-	# Remember partitions
-	export PART_EFI="${DRIVE_ROOT}1";
-	export PART_BOOT="${DRIVE_ROOT}2";
-	export PART_SYSTEM="${DRIVE_ROOT}3";
-	export PART_SYSTEM_NUM="3";
-else
-	logLine "Using BIOS partition scheme..."
-	sfdisk -q ${DRIVE_ROOT} &> /dev/null <<- EOM
+sfdisk -q ${DRIVE_ROOT} &> /dev/null <<- EOM
 label: gpt
 unit: sectors
 
 start=2048, size=20480, type=21686148-6449-6E6F-744E-656564454649, bootable
-start=22528, size=512000, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="boot"
-start=534528, size=204800, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="system"
+start=22528, size=204800, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="efi"
+start=227328, size=512000, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="boot"
+start=739328, size=512000, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="system"
 EOM
 
-	# Check Result
-	if [ $? -ne 0 ]; then
-		logLine "Failed to partition ROOT-Drive"
-		exit
-	fi;
-
-	# Remember partitions
-	export PART_EFI=""
-	export PART_BOOT="${DRIVE_ROOT}2"
-	export PART_SYSTEM="${DRIVE_ROOT}3"
-	export PART_SYSTEM_NUM="3"
+# Check Result
+if [ $? -ne 0 ]; then
+	logLine "Failed to partition ROOT-Drive"
+	exit
 fi;
+
+# Remember partitions
+export PART_EFI="${DRIVE_ROOT}2"
+export PART_BOOT="${DRIVE_ROOT}3"
+export PART_SYSTEM="${DRIVE_ROOT}4"
+export PART_SYSTEM_NUM="4"
 
 if ! runCmd parted -s ${DRIVE_ROOT} resizepart ${PART_SYSTEM_NUM} 100%; then logLine "Failed to expand ROOT-Partition"; exit; fi;
 
