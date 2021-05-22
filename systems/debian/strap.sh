270a14afd5cc95d6f94f02f47e1ddb15d9ab9085
EgoFelix <git@egofelix.de>
2021-05-22 11:18:06 +0200

    Work

--- systems/debian/strap.sh
+++ systems/debian/strap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if ! runCmd debootstrap stable /tmp/mnt/root; then
+if ! runCmd debootstrap stable /tmp/mnt/root http://httpredir.debian.org/debian/; then
   logError "Failed to install Base-System"; exit 1;
 fi;
