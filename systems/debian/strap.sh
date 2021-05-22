3b99ee15910833a741897d580a0ed4427be5fea1
EgoFelix <git@egofelix.de>
2021-05-22 12:11:42 +0200

    Work

--- systems/debian/strap.sh
+++ systems/debian/strap.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-if ! runCmd debootstrap stable /tmp/mnt/root http://httpredir.debian.org/debian/; then
-  logError "Failed to install Base-System"; exit 1;
+if ! runCmd debootstrap stable /tmp/mnt/root; then
+  # Guess arch
+  ARCH=$(uname -a)
+
+  if [[ $ARCH = "x86_64" ]]; then
+    ARCH="amd64"
+  else
+    logError "Failed to guess arch"; exit 1;
+  fi;
+
+  
+  if ! runCmd debootstrap --arch $ARCH stable /tmp/mnt/root; then
+    logError "Failed to install Base-System"; exit 1;
+  fi;
 fi;
