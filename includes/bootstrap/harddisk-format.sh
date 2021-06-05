4569d19a42933faed361c2d1c6e1278b56282d6f
Felix <git@egofelix.de>
2021-06-05 14:25:00 +0200

    Work

--- includes/bootstrap/harddisk-format.sh
+++ includes/bootstrap/harddisk-format.sh
@@ -46,9 +46,9 @@ function harddisk-format {
     if [[ -z $(echo "${RUNCMD_CONTENT}" | grep "PARTLABEL=\"system\"") ]]; then NEEDS_PARTITIONING="true"; fi;
 
     if isTrue ${NEEDS_PARTITIONING}; then
-    # Format drives
-    logLine "Partitioning ${HARDDISK} with default partition scheme (bios and efi support)...";
-    sfdisk -q ${HARDDISK} &> /dev/null <<- EOM
+        # Format drives
+        logLine "Partitioning ${HARDDISK} with default partition scheme (bios and efi support)...";
+        sfdisk -q ${HARDDISK} &> /dev/null <<- EOM
 label: gpt
 unit: sectors
 
@@ -57,25 +57,40 @@ start=22528, size=204800, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="efi"
 start=227328, size=512000, type=BC13C2FF-59E6-4262-A352-B275FD6F7172, name="boot"
 start=739328, size=512000, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="system"
 EOM
-    
-    # Check Result
-    if [ $? -ne 0 ]; then
-        logLine "Failed to partition ${HARDDISK}";
-        return 1;
-    fi;
-    
-    # Remember partitions
-    export PART_EFI="${HARDDISK}2"
-    export PART_BOOT="${HARDDISK}3"
-    export PART_SYSTEM="${HARDDISK}4"
-    export PART_SYSTEM_NUM="4"
-    
-    if ! runCmd parted -s ${DRIVE_ROOT} resizepart ${PART_SYSTEM_NUM} 100%; then logError "Failed to expand ROOT-Partition"; return 1; fi;
-    
-    # Sync drives
-    sleep 1
-    sync
-    sleep 1
+        
+        # Check Result
+        if [ $? -ne 0 ]; then
+            logLine "Failed to partition ${HARDDISK}";
+            return 1;
+        fi;
+        
+        # Remember partitions
+        export PART_EFI="${HARDDISK}2"
+        export PART_BOOT="${HARDDISK}3"
+        export PART_SYSTEM="${HARDDISK}4"
+        export PART_SYSTEM_NUM="4"
+        
+        if ! runCmd parted -s ${DRIVE_ROOT} resizepart ${PART_SYSTEM_NUM} 100%; then logError "Failed to expand ROOT-Partition"; return 1; fi;
+        
+        # Sync drives
+        sleep 1
+        sync
+        sleep 1
+
+        # Format EFI-Partition
+        if [[ ! -z "${PART_EFI}" ]]; then
+            logLine "Formatting EFI-Partition (${PART_EFI})...";
+            if ! runCmd mkfs.vfat -F32 ${PART_EFI}; then logError "Failed to Format EFI-Partition."; return 1; fi;
+            if ! runCmd fatlabel ${PART_EFI} EFI; then logError "Failed to label EFI-Partition."; return 1; fi;
+        fi;
+
+        # Format BOOT-Partition
+        logLine "Formatting BOOT-Partition (${PART_BOOT})...";
+        if ! runCmd mkfs.ext2 -F -L boot ${PART_BOOT}; then logError "Failed to format BOOT-Partition"; return 1; fi;
+
+        # Format Partition
+        logLine "Formatting SYSTEM-Partition";
+        if ! runCmd mkfs.btrfs -f -L system ${PART_SYSTEM}; then logError "Failed to format SYSTEM-Partition"; exit 1; fi;
     fi;
     return 0;
 }
