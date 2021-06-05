9278b4b079859e618a0e4447b2b8935eab23c5f6
Felix <git@egofelix.de>
2021-06-05 17:13:26 +0200

    asdasd

--- scripts/chroot_prepare.sh
+++ scripts/chroot_prepare.sh
@@ -5,9 +5,8 @@ if ! runCmd mount -t proc proc /tmp/mnt/root/proc; then logLine "Error preparing
 if ! runCmd mount -t sysfs sys /tmp/mnt/root/sys; then logLine "Error preparing chroot"; exit; fi;
 if ! runCmd mount -t devtmpfs dev /tmp/mnt/root/dev; then logLine "Error preparing chroot"; exit; fi;
 if ! runCmd mount -t devpts devpts /tmp/mnt/root/dev/pts; then logLine "Error preparing chroot"; exit; fi;
-if isEfiSystem; then
-  if ! runCmd mount -t efivarfs efivarfs /tmp/mnt/root/sys/firmware/efi/efivars; then logLine "Error preparing chroot"; exit; fi;
-fi;
+
+runCmd mount -t efivarfs efivarfs /tmp/mnt/root/sys/firmware/efi/efivars;
 
 # Install current resolv.conf
 if ! runCmd cp /etc/resolv.conf /tmp/mnt/root/etc/resolv.conf; then logLine "Error preparing chroot"; exit; fi;
