42092340d30a520de5131634bb0507e55002bf5c
Felix <git@egofelix.de>
2021-06-05 12:44:17 +0200

    Work

--- commands/system-manager/bootstrap.sh
+++ commands/system-manager/bootstrap.sh
@@ -24,7 +24,7 @@ function run {
     
     # Test if we are running a live iso
     local IS_LIVE="false";
-    if ! runCmd "findmnt -n / -r"; then logError "Could not detect rootfs type"; exit 1; fi;
+    if ! runCmd findmnt -n / -r; then logError "Could not detect rootfs type"; exit 1; fi;
     local ROOTFS_TYPE=$(echo "${RUNCMD_CONTENT,,}" | cut -d' ' -f 2);
     case "${ROOTFS_TYPE,,}" in
         airootfs) IS_LIVE="true";;
