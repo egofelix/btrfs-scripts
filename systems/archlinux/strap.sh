90cc79d7dfe68abdf43842775a02c6188caae1e0
Felix <git@egofelix.de>
2021-06-05 18:08:49 +0200

    arch cache

--- systems/archlinux/strap.sh
+++ systems/archlinux/strap.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+if [[ ! -f "/etc/pacman.d/mirrorlist.bak" ]]; then
+    cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+fi;
+
+echo "https://cached.egofelix.de/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+
 if ! runCmd pacstrap /tmp/mnt/root base; then
-  if ! runCmd /tmp/arch-bootstrap.sh /tmp/mnt/root; then
-    logError "Failed to install Base-System"; exit 1;
-  fi;
+    if ! runCmd /tmp/arch-bootstrap.sh /tmp/mnt/root; then
+        logError "Failed to install Base-System"; exit 1;
+    fi;
 fi;
