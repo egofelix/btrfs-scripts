cfa48fe45062a4b7e9562c43097c52299dfc4c92
Felix <git@egofelix.de>
2021-06-06 15:24:12 +0200

    better fdns

--- includes/dns.sh
+++ includes/dns.sh
@@ -20,25 +20,37 @@ function dnsResolveSrv {
     
     # Try With nslookup
     if ! which nslookup &> /dev/null; then
-        if ! runCmd nslookup -type=srv ${HOSTNAME}; then
-            logDebug "Failed to lookup ${HOSTNAME}, type \"SRV\", cmd \"nslookup\"";
-            return 1;
+        if runCmd nslookup -type=srv ${HOSTNAME}; then
+            if [[ "${RUNCMD_CONTENT}" = *"NXDOMAIN"* ]]; then
+                logDebug "${HOSTNAME} does not exist.";
+                return 1;
+            fi;
+            
+            local SERVICE=$(echo ${RUNCMD_CONTENT} | grep -oE '\s+service\s+\=\s+[0-9]+\s+[0-9]+\s+[0-9]+\s+[A-Za-z\.0-9]+');
+            if [[ -z "${SERVICE}" ]]; then
+                return 1;
+            fi;
+            
+            DNS_PORT=$(echo ${SERVICE}  | awk '{print $5}')
+            DNS_HOSTNAME=$(echo ${SERVICE}  | awk '{print $6}')
+            DNS_HOSTNAME="${DNS_HOSTNAME::-1}";
+            return 0;
         fi;
-        
-        if [[ "${RUNCMD_CONTENT}" = *"NXDOMAIN"* ]]; then
-            logDebug "${HOSTNAME} does not exist.";
-            return 1;
+    fi;
+    
+    # Try with resolvectl
+    if ! which resolvectl &> /dev/null; then
+        if runCmd resolvectl query -t SRV ${HOSTNAME} --legend=false; then
+            local SERVICE=$(echo ${RUNCMD_CONTENT} | grep -oE '\s+SRV\s+[0-9]+\s+[0-9]+\s+[0-9]+\s+[A-Za-z\.0-9]+');
+            if [[ -z "${SERVICE}" ]]; then
+                return 1;
+            fi;
+            
+            DNS_PORT=$(echo ${SERVICE}  | awk '{print $4}')
+            DNS_HOSTNAME=$(echo ${SERVICE}  | awk '{print $5}')
+            DNS_HOSTNAME="${DNS_HOSTNAME::-1}";
+            return 0;
         fi;
-        
-        local SERVICE=$(echo ${RUNCMD_CONTENT} | grep -oE '\s+service\s+\=\s+[0-9]+\s+[0-9]+\s+[0-9]+\s+[A-Za-z\.0-9]+');
-        if [[ -z "${SERVICE}" ]]; then
-            return 1;
-        fi;
-        
-        DNS_PORT=$(echo ${SERVICE}  | awk '{print $5}')
-        DNS_HOSTNAME=$(echo ${SERVICE}  | awk '{print $6}')
-        DNS_HOSTNAME="${DNS_HOSTNAME::-1}";
-        return 0;
     fi;
     
     # Try with dig
