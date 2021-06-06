3b96d03e75ab6f731a5f0a63e835fb2465c4c36b
Felix <git@egofelix.de>
2021-06-06 11:08:03 +0200

    asdasd

--- scripts/bootmanager.sh
+++ scripts/bootmanager.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Todo, detect system here
-SYSTEMID=$(cat /mnt/tmp/root/etc/os-release | grep "^ID=");
+SYSTEMID=$(cat /tmp/mnt/root/etc/os-release | grep "^ID=");
 
-if [[ "${SYSTEMID^^}" = *"DEBIAN"* ]]; then
-  source "${BASH_SOURCE%/*}/../systems/debian/bootmanager.sh";
-elif [[ "${SYSTEMID^^}" = *"ARCHLINUX"* ]]; then
-  source "${BASH_SOURCE%/*}/../systems/archlinux/bootmanager.sh";
+if [[ "${SYSTEMID^^}" = *"DEBIAN"* ]];
+then
+    source "${BASH_SOURCE%/*}/../systems/debian/bootmanager.sh";
+elif [[ "${SYSTEMID^^}" = *"ARCHLINUX"* ]];
+then
+    source "${BASH_SOURCE%/*}/../systems/archlinux/bootmanager.sh";
 else
-  logError "Unable to detect system, skipping bootmanager!";
+    logError "Unable to detect system, skipping bootmanager!";
 fi;
