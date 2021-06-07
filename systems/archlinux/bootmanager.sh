e642901a0b1af3b959d186d30977ed8c3f2b0259
Felix <git@egofelix.de>
2021-06-07 09:09:36 +0200

    sdfsdf

--- systems/archlinux/bootmanager.sh
+++ systems/archlinux/bootmanager.sh
@@ -56,7 +56,7 @@ if isTrue ${USE_SYSTEMD_INIT};
 then
     # Setup systemd hooks
     HOOKS="(base autodetect modconf block filesystems keyboard fsck systemd systemd-tool)";
-    sed -i "s/HOOKS=.*/${HOOKS}/g" /tmp/mnt/root/etc/mkinitcpio.conf;
+    sed -i "s/HOOKS=.*/HOOKS=${HOOKS}/g" /tmp/mnt/root/etc/mkinitcpio.conf;
     
     # Install systemd-tools and configure
     cat > /tmp/mnt/root/chroot.sh <<- EOF
