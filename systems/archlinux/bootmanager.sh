e30f3710918132daf3d2a34691447b494c3c9071
Felix <git@egofelix.de>
2021-06-08 09:47:26 +0200

    shutdown hook for usr

--- systems/archlinux/bootmanager.sh
+++ systems/archlinux/bootmanager.sh
@@ -134,7 +134,7 @@ EOF
     
     # Add usr hook to mkinitcpio.conf if usr is on a subvolume
     if [[ ! -z $(LANG=C mount | grep ' /tmp/mnt/root/usr type ') ]]; then
-        HOOKS="HOOKS=($(source /tmp/mnt/root/etc/mkinitcpio.conf && if [[ ${HOOKS[@]} != *"usr"* ]]; then HOOKS+=(usr); fi && echo ${HOOKS[@]} | xargs echo -n))"
+        HOOKS="HOOKS=($(source /tmp/mnt/root/etc/mkinitcpio.conf && if [[ ${HOOKS[@]} != *"usr"* ]]; then HOOKS+=(usr); HOOKS+=(shutdown); fi && echo ${HOOKS[@]} | xargs echo -n))"
         sed -i "s/HOOKS=.*/${HOOKS}/g" /tmp/mnt/root/etc/mkinitcpio.conf
     fi;
 else
